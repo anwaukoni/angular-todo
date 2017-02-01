5aa3437857629d11a14b636299bdf0d0fec04323
anwaukoni <anwaukoni@gmail.com>
2017-02-01 15:46:17 +0100

    created delete button

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,18 +13,17 @@ import {Component} from "@angular/core";
     </header>
       <nav class="navbar navbar-inverse col-sm-3 todoListNav" [class.invisible]="listEmpty()">
         <ul class="list-group" style="margin-top:1em">
-          <li style="cursor:pointer" class="list-group-item" *ngFor="let list of lists; let i = index"
-          (click)= "deleteTask(i)"
-          >
+          <li style="cursor:pointer" class="list-group-item" *ngFor="let list of lists; let i = index">
             {{list}}
+            <span (click)= "deleteTask(i)" style="float:right"> X </span>
           </li>
         </ul>
 
       </nav>
     <div class="jumbotron col-sm-9">
       <div >
-      <input #task type="text"  class="text-center col-sm-12" style="color: white; background-color:transparent; border-width:0 0 1px 0; padding: 0.5em; margin-bottom:15px;" placeholder="New Task" #Task/>
-      <button (click)="addTask(task.value)" class="text-center btn btn-success" style="padding-right:5em; padding-left:5em; ">Submit</button>
+      <input #task type="text"  class="text-center col-sm-12" style="color: white; background-color:transparent; border-width:0 0 1px 0; padding: 0.5em; margin-bottom:15px;" placeholder="New Task"/>
+      <button (click)="addTask(task.value)" class="text-center btn btn-success" style="padding-right:5em; padding-left:5em; ">Add</button>
         <!--form class="form" (submit)=addTask(Task.value);>
             <textarea class="col-sm-12" placeholder="Say more about your task" style="background-color:transparent; border-width:0 0 1px 0; padding: 0.5em; background-color:rgba(230,230, 230, 0.2); color[placeholder]:white; margin-bottom:1em;">
             </textarea>
@@ -52,14 +51,18 @@ import {Component} from "@angular/core";
 export class AppComponent {
 
   lists= ["Task1","Task2","Task3"];
+
   addTask(task: string){
     if(task){
       this.lists.push(task)
     }
+
+
+
     console.log(task);
   }
 
-  deleteTask(index){
+  deleteTask(index:number){
     this.lists.splice(index,1);
   }
 
